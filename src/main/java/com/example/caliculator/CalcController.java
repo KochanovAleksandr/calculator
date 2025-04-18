f457881943bdffa75263552611bba6ed90f57cc7
KochanovAA <KochanovAleksandr@gmail.com>
2025-04-18 14:00:03 +0400

    ver 1.0

--- src/main/java/com/example/caliculator/CalcController.java
+++ src/main/java/com/example/caliculator/CalcController.java
@@ -14,18 +14,16 @@ public class CalcController {
 	
 	
    @GetMapping
-   public String calculate(	@RequestParam( name = "averageSalary",required = false,defaultValue = "0") double averageSalary,
-    						@RequestParam(name = "numberOfDays",required = false)Integer numberOfDays,
+   public String calculate(	@RequestParam( name = "averageSalary",required = false,defaultValue = "0") Double averageSalary,
     						@RequestParam(name = "startDay",required = false)LocalDate startDay,
     						@RequestParam(name = "stopDay", required = false) LocalDate stopDay,
     						Model model){
 	   System.out.println(startDay);
 	   System.out.println(stopDay);
-	   System.out.println(numberOfDays);
-	   System.out.println(new Calculator(averageSalary,numberOfDays).getResult());
+
     	
 		
-        model.addAttribute("vacationPayments", String.format("%.2f", new Calculator(averageSalary,numberOfDays).getResult()));
+        model.addAttribute("vacationPayments", String.format("%.2f", new Calculator(averageSalary,startDay,stopDay).getResult()));
         
                 return "calculate";
     }
